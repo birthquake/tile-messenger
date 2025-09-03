e107d4c642cee935945cd3aeacb0a915962c5472
birthquake <rashiedtyre@gmail.com>
2025-09-03 03:05:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 // src/App.js
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 import TileGrid from './components/TileGrid';
-import ConversationView from './components/ConversationView';
-import './App.css'; // Global styles
+import AuthForm from './components/AuthForm';
 
 function App() {
-  const [selectedThread, setSelectedThread] = useState(null);
+  const [user, setUser] = useState(null);
 
-  const handleTileTap = (tile) => {
-    const mockThread = {
-      id: tile.id,
-      title: tile.preview || 'Conversation',
-      messages: [] // Will be populated in ConversationView
-    };
-    setSelectedThread(mockThread);
-  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+    });
+    return () => unsubscribe();
+  }, []);
 
-  const handleBack = () => {
-    setSelectedThread(null);
-  };
+  if (!user) {
+    return <AuthForm onAuth={setUser} />;
+  }
 
-  return (
-    <div className="app">
-      {selectedThread ? (
-        <ConversationView thread={selectedThread} onBack={handleBack} />
-      ) : (
-        <TileGrid onTileTap={handleTileTap} />
-      )}
-    </div>
-  );
+  return <TileGrid onTileTap={(tile) => console.log('Tapped:', tile)} />;
 }
 
 export default App;
